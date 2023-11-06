950cea528976fcba58b7caac6caf320cb0153f57
Tharki-God <alokender018@gmail.com>
2023-11-06 07:29:43 +0530

    better

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
-import { Logger } from "replugged";
-import { fluxDispatcher, toast } from "replugged/common";
+import { Logger, util } from "replugged";
+import { toast } from "replugged/common";
 const PluginLogger = Logger.plugin("PluginTemplate");
 
-export function start(): void {
-  function hug(): void {
-    const stylesheets = document.querySelectorAll(
-      'link[rel="stylesheet"][href]:not([href^="replugged://"])',
-    );
-    stylesheets.forEach(function (existingLink: HTMLLinkElement) {
-      const parent = existingLink.parentNode;
-      const newLink = document.createElement("link");
-      newLink.href = existingLink.href;
-      newLink.rel = "stylesheet";
-      parent.replaceChild(newLink, existingLink);
-    });
-    toast.toast("baby girl don't cri, you wouldn't crash now.");
-    PluginLogger.error("There you are mama. Don't worry its fine.");
-    fluxDispatcher.unsubscribe("RPC_SERVER_READY", hug);
-  }
-  fluxDispatcher.subscribe("RPC_SERVER_READY", hug);
+export async function start(): Promise<void> {
+  await util.waitFor('[data-list-id="guildsnav"]');
+  const stylesheets = document.querySelectorAll(
+    'link[rel="stylesheet"][href]:not([href^="replugged://"])',
+  );
+  stylesheets.forEach(function (existingLink: HTMLLinkElement) {
+    const parent = existingLink.parentNode;
+    const newLink = document.createElement("link");
+    newLink.href = existingLink.href;
+    newLink.rel = "stylesheet";
+    parent.replaceChild(newLink, existingLink);
+  });
+  toast.toast("baby girl don't cri, you wouldn't crash now.");
+  PluginLogger.error("There you are mama. Don't worry its fine.");
 }
 
 export function stop(): void {
